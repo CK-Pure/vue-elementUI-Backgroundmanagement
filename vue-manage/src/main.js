fcab50efef6a963d8f0647676575c3ac7dcb07dd
CK-Pure <akashi_hsw@163.com>
2022-03-23 21:38:15 +0800

    code

--- vue-manage/src/main.js
+++ vue-manage/src/main.js
@@ -1,6 +1,7 @@
 // @ts-nocheck
 import Vue from 'vue'
 import App from '../src/App.vue'
+import router from '../router'
 
 // 引入elementUI
 import ElementUI from 'element-ui'
@@ -12,5 +13,6 @@ Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
